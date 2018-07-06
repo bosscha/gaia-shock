aa6722c263231495f00bed27f662e42922f9b61c
stephane <steph.leon@gmail.com>
2018-07-06 19:12:21 -0400

    adding new test

--- src/runTest.py
+++ src/runTest.py
@@ -11,7 +11,7 @@ from gaia.tap import cone_search
 
 
 c = coord.SkyCoord.from_name("M67")
-data = cone_search(c.ra.deg, c.dec.deg, 0.5, table="gaiadr2.gaia_source")
+data = cone_search(c.ra.deg, c.dec.deg, 0.5, table="gaiadr2.gaia_source", dump_to_file=True)
 
 gaia = gu.gaiaSet(data)
 
