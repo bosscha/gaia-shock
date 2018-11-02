71c26b1280bd7c1ec6261106b6c36261d37a56f3
stephane <steph.leon@gmail.com>
2018-11-02 09:54:27 +0100

    add install.jl script to install the julia package needed for GaiaClustering

---
+++ src/install.jl
@@ -0,0 +1,13 @@
+## install package for Gaia.Clustering
+
+using Pkg
+
+Pkg.add("PyCall")
+Pkg.add("DataFrames")
+Pkg.add("Clustering")
+Pkg.add("Statistics")
+Pkg.add("Distributions")
+Pkg.add("Random")
+
+println("## Package installation for GaiaClustering done...")
+
