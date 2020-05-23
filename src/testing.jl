c5fe85393dc195f66ee88b54339a05de681fdb6e
stephane <steph.leon@gmail.com>
2020-05-23 10:40:12 -0400

    updt

--- src/testing.jl
+++ src/testing.jl
@@ -1,20 +1,19 @@
 ## testing function (for notebooks e.g.)
 
-function __plot_check(dfcart,plotdir,plotfile)
+function __plot_check(dfcart,plotdir,plotfile, showplot=true)
     cart= DataFrame(X=dfcart.data[1,:], Y=dfcart.data[2,:], Z=dfcart.data[3,:])
 
     println("## check plot subtraction ...")
 
     PyPlot.plt.figure(figsize=(9.0,8.0))
-    PyPlot.plt.subplot(1, 1, 1 , ylim=[100,450], xlim=[-50,50])
+    PyPlot.plt.subplot(1, 1, 1 , xlim=[-100,100])
     PyPlot.plt.scatter(cart.Y, cart.X, s = 0.1 )
     PyPlot.plt.xlabel("Y (pc)")
     PyPlot.plt.ylabel("X (pc)")
     PyPlot.plt.grid(true)
 
     PyPlot.plt.savefig(plotdir*"/"*plotfile)
-    PyPlot.plt.show()
-
+    if showplot PyPlot.plt.show() end
 end
 
 function __plot_nstars(nstarh,plotfile="test-stats-votable.png", plotdir= ".")
